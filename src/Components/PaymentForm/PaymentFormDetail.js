1445fd87132a36d96165423ff0d9724d6ae30a33
Your Name <fptaptecht2404e@gmail.com>
2025-03-04 19:16:08 +0700

    add

--- src/Components/PaymentForm/PaymentFormDetail.js
+++
@@ -1,7 +0,0 @@
-
-
-
-
-
-
-    const userId = localStorage.getItem('userId');  useEffect(() => {const PaymentFormDetail = () => {import './PaymentFormDetail.css';import React, { useEffect } from 'react';
