74116835f501ccdb361023905dff9ba8e1e97e2d
bemixzyy <bolunlun1210@gmail.com>
2025-03-04 20:44:07 +0700

    update img

--- src/Components/MembershipSection/MembershipSection.tsx
+++ src/Components/MembershipSection/MembershipSection.tsx
@@ -6,25 +6,25 @@ const MembershipSection: React.FC = () => {
     <div
       className="membership-container"
       style={{
-        backgroundImage: `url('https://cinestar.com.vn/_next/image/?url=%2Fassets%2Fimages%2Fbg-cfriends.webp&w=1920&q=75')`,
+        backgroundImage: `url('https://skystarimages.s3.us-east-1.amazonaws.com/banner/cinema.JPG')`,
       }}
     >
       <div className="membership-inner">
         <h2 className="membership-title">CHƯƠNG TRÌNH THÀNH VIÊN</h2>
 
         <div className="membership-card-container">
-          {/* Thẻ thành viên C'Friend */}
+          {/* Thẻ thành viên S'Friend */}
           <div className="membership-card">
             <div className="membership-card-image">
               <img
-                src="https://api-website.cinestar.com.vn/media/wysiwyg/CMSPage/Member/Desktop519x282_CMember.webp"
-                alt="C'Friend"
+                src="https://skystarimages.s3.us-east-1.amazonaws.com/products/sfrs.JPG"
+                alt="S'Friend"
               />
             </div>
             <div className="membership-card-content">
-              <h3 className="membership-card-title">THÀNH VIÊN C'FRIEND</h3>
+              <h3 className="membership-card-title">THÀNH VIÊN S'FRIEND</h3>
               <p className="membership-card-description">
-                Thẻ C'Friend nhiều ưu đãi cho thành viên mới
+                Thẻ S'Friend nhiều ưu đãi cho thành viên mới
               </p>
               <div className="membership-button-wrapper">
                 <div className="membership-button-gradient" />
@@ -37,14 +37,14 @@ const MembershipSection: React.FC = () => {
           <div className="membership-card">
             <div className="membership-card-image">
               <img
-                src="https://api-website.cinestar.com.vn/media/wysiwyg/CMSPage/Member/c-vip.webp"
-                alt="C'VIP"
+                src="https://skystarimages.s3.us-east-1.amazonaws.com/products/svip.JPEG"
+                alt="S'VIP"
               />
             </div>
             <div className="membership-card-content">
-              <h3 className="membership-card-title">THÀNH VIÊN C'VIP</h3>
+              <h3 className="membership-card-title">THÀNH VIÊN S'VIP</h3>
               <p className="membership-card-description">
-                Thẻ VIP CineStar mang đến sự ưu đãi đặc quyền
+                Thẻ VIP SkyStar mang đến sự ưu đãi đặc quyền
               </p>
               <div className="membership-button-wrapper">
                 <div className="membership-button-gradient" />
