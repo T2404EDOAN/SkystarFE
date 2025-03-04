b7d7226a698ee1546d5035eb5fb18e0046722921
bemixzyy <bolunlun1210@gmail.com>
2025-03-04 20:23:48 +0700

    link ent

--- src/Components/EntertainmentSection/EntertainmentSection.tsx
+++ src/Components/EntertainmentSection/EntertainmentSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./EntertainmentSection.css";
+import { Link } from "react-router-dom";
 
 const EntertainmentSection: React.FC = () => {
   return (
@@ -17,34 +18,43 @@ const EntertainmentSection: React.FC = () => {
             {
               src: "https://cinestar.com.vn/assets/images/img-service0.webp",
               alt: "Kidzone",
+              link: "/khu-tre-em",
+              
             },
             {
               src: "https://cinestar.com.vn/assets/images/img-service1.webp",
               alt: "Bowling",
+              link: "/bowling",
             },
             {
               src: "https://cinestar.com.vn/assets/images/img-service2.webp",
               alt: "Billiards",
+              link: "/billiard",
             },
             {
               src: "https://cinestar.com.vn/assets/images/img-service3.webp",
               alt: "Món ngon",
+              link: "/nha-hang",
             },
             {
               src: "https://cinestar.com.vn/assets/images/img-service4.webp",
               alt: "Gym",
+              link: "/gym",
             },
             {
               src: "https://cinestar.com.vn/assets/images/img-service5.webp",
               alt: "Opera",
-            },
+              link: "/opera",
+            }, 
           ].map((item, index) => (
             <div key={index} className="entertainment-grid-item">
+              <Link to={item.link}>
               <img
                 src={item.src}
                 alt={item.alt}
                 className="entertainment-image"
               />
+              </Link>
             </div>
           ))}
         </div>
