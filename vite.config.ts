15662b743fc92eeeb2e2aa55975451463d03d4bd
hieuvm <fptaptecht2404e@gmail.com>
2025-03-10 21:45:11 +0700

    fx https

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import fs from 'fs';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173, // Cổng mặc định
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+    }, // Bật HTTPS nếu cần
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // Proxy API requests đến BE
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
